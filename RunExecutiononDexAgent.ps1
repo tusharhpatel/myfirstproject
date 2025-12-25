16a4e6704c6de55639cd6d689c73ffa4db5698f0
tusharhpatel <54415012+tusharhpatel@users.noreply.github.com>
2025-12-25 15:04:15 -0600

    Add files via upload

---
+++ RunExecutiononDexAgent.ps1
@@ -0,0 +1,4 @@
+cd  C:\Testing\ToscaExecutionClient\ToscaExecutionClient 
+
+.\tosca_execution_client.ps1 -toscaServerUrl "https://azusea1-tostap1.covetrus.net" -projectName "CovetrusTechGroup1" -events '["Sample"]' -clientId "PNwiiGLackiViiNS5YtVrA" -clientSecret "vCqlYo1gu0SIeMxZr5dAdAtZQP1YgGtEWg9SKkAveaMg" -executionEnvironment "Dex" 
+
